63c94ea0c5ba8f14043cd5dc8077e9ec2a488e0e
hamlet <dimash@MacBook-Pro-Dimas.local>
2021-11-09 22:43:44 +0600

    updates

--- src/components/Blank.js
+++ src/components/Blank.js
@@ -1,4 +1,7 @@
-import NavBar from "./Navbar";
+import { faMap, faChartBar } from "@fortawesome/free-regular-svg-icons";
+import { faHistory } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 const BackPage = () => {
     return (
@@ -15,19 +18,38 @@ const Blank = () => {
             <div className="blankFrames__item_1" style={{ background: "#f7f4f5" }}>
                 <div className="navbar__item_1">
                     <span style={{ position: "absolute", bottom: "1em", paddingLeft: "1em", fontWeight: 'bold', fontSize: 24 }}>
-                        Dashboard
+                        Дэшборд
                     </span>
                 </div>
                 <div className="navbar__item_2">
                     <div className="navLeftList">
-                        <ul className="ulListLeft">
-                            <li>1</li>
-                            <li>1</li>
-                            <li>1</li>
+                        <ul className="ulListLeft" style={{ marginTop: "9em" }}>
+                            <li className="ul__item">
+                                <div className="leftNavItems" style={{ display: 'grid', gridTemplateColumns: '1fr 2fr', gridGap: "1em", alignItems: 'center' }}>
+                                    <FontAwesomeIcon icon={faChartBar} size="lg" />
+                                    <span style={{ color: "gray", fontWeight: 600 }}>Дэшборд</span>
+                                </div>
+                            </li>
+                            <li className="ul__item">
+                                <div className="leftNavItems" style={{ display: 'grid', gridTemplateColumns: '1fr 2fr', gridGap: "1.95em", alignItems: 'center' }}>
+                                    <FontAwesomeIcon icon={faMap} size="lg" />
+                                    <span style={{ color: "gray", fontWeight: 600 }}>Карта</span>
+                                </div>
+                            </li>
+
                         </ul>
                     </div>
                     <div className="navHelpPopup">
-
+                        <div className="leftPopup" style={{ display: "grid", gridTemplateRows: "1fr 2fr", gridGap: "1em" }}>
+                            <div style={{ paddingLeft: "2.3em", paddingTop: "2em", display: "flex", justifyContent: "left" }}>
+                                <FontAwesomeIcon icon={faHistory} style={{ color: "white" }} size="lg" />
+                                <div></div>
+                            </div>
+                            <div style={{ display: "flex", justifyContent: "left", gap: "1em", textAlign: "start", paddingLeft: "2.3em", flexDirection: "column" }}>
+                                <span style={{ color: "white", fontWeight: "bolder" }}><strong>История доступна</strong></span>
+                                <span style={{ color: "white", fontSize: ".7em" }}>Просмотрите историй преступлений</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
